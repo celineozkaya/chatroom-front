3eecff0a9f5f8644910a3743aca0194cc484bd81
Celine Ozkaya <celine.ozkaya@etu.utc.fr>
2025-06-08 22:18:18 +0200

    CreateChat.tsx : multiselect avec data du back ok

--- src/pages/CreateChat.tsx
+++ src/pages/CreateChat.tsx
@@ -1,9 +1,25 @@
-import type { CSSProperties, JSX } from "react";
-import style from "../styles/Accueil.module.css"
+import { useState, type CSSProperties, type JSX, useEffect } from "react";
+import style from "../styles/Accueil.module.css";
+import axios from "axios";
+import { MultiSelect } from 'primereact/multiselect';
+import 'primereact/resources/themes/lara-light-indigo/theme.css'; // ou autre thème
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
 
 interface CreateChatProps{
 
 }
+
+interface User{
+    readonly id : number; 
+    readonly firstname : string; 
+    readonly lastname : string; 
+    readonly email : string; 
+    // readonly avatar : string; // c'est une photo
+}
+
+
 
 const formStyle : CSSProperties = {
     display: "flex", 
@@ -20,7 +36,7 @@ const inputStyle : CSSProperties = {
     fontSize : "16px", 
     border : "1px solid lightgrey", 
     borderRadius : "20px", 
-    height : "20px",
+    height : "40px",
 }
 
 const selectStyle : CSSProperties = {
@@ -38,35 +54,74 @@ const selectStyle : CSSProperties = {
 
 
 export default function CreateChat({} : CreateChatProps) : JSX.Element {
+    // pour peupler le select (lister les utilisateurs)
+    const [users, setUsers] = useState<User[]>([])
+    const [selectedUsers, setSelectedUsers] = useState<User[]>([]);
+
+
+    // au chargement du composant, recuperer la liste des utilisateurs
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    // requete get pour la liste des users
+    function getUsers() {
+        axios
+            .get("http://localhost:8080/api/users")
+            .then((res) => {
+                // caster la liste de liste en liste de user
+                const tempUsers : User[] = res.data.map((user : (number | string)[]) => ({
+                    id: user[0] as number,
+                    firstname: user[1] as string,
+                    lastname: user[2] as string,
+                    email: user[3] as string
+                }));
+                setUsers(tempUsers);
+            })
+            .catch((err) => console.error(err));
+    };
+
     function createChat(formData : FormData) {
         
     }
+
+    // template pour une option du multiselect
+    const userTemplate = (option : User) => {
+        return (
+            <div className="flex align-items-center">
+                <div>{option.firstname} {option.lastname} ({option.email})</div>
+            </div>
+        );
+    };
+
 
     return (
         <div style={{display: "flex", flexDirection: "column", alignItems : "center"}}>
             <div style={{fontSize :"26px", padding : "20px"}}>Créez un chat</div>
             <form action={createChat} style={formStyle}>
-                <div>Nom du chat</div> 
-                <input type="text" name="name" style={inputStyle}/>
+                <div>Nom du chat*</div> 
+                <input type="text" name="name" style={inputStyle} required/>
 
-                <div>Description</div> 
-                <input type="text" name="description" style={inputStyle}/>
+                <div>Description*</div> 
+                <input type="text" name="description" style={inputStyle} required/>
 
                 <div style={{display: "flex", gap : "20px"}}> 
                     <div style={{display: "flex", flexDirection: "column", gap: "10px"}}>
-                        Date
-                        <input type="date" style={{...inputStyle, width: "fit-content"}}/>
+                        Date*
+                        <input type="date" style={{...inputStyle, width: "fit-content"}} required/>
                     </div> 
                     <div style={{display: "flex", flexDirection: "column", gap: "10px"}}>
-                        Période de validité (en jours)
-                        <input type="number" min="0" style={inputStyle}/>
+                        Période de validité (en jours)*
+                        <input type="number" min="0" style={inputStyle} required/>
                     </div>
                 </div>
 
-                <div>Invités</div> 
-                <select style={selectStyle} multiple>
+                <div>Invités*</div> 
+                <MultiSelect value={selectedUsers} onChange={(e) => setSelectedUsers(e.value)} options={users} optionLabel="email"
+                placeholder="Selectionnez vos invités" itemTemplate={userTemplate} display="chip" className="w-full" />
 
-                </select>
+                <small>*Champs requis.</small>
+
                 <div style={{display : "flex", justifyContent : "center"}}>
                     <button type="submit" className={style.createchat} style={{border : "none"}}>Créer mon chat</button>
                 </div>
@@ -76,3 +131,7 @@ export default function CreateChat({} : CreateChatProps) : JSX.Element {
         
     );
 }
+
+// https://medium.com/nerd-for-tech/fetching-api-using-useeffect-hook-in-react-js-7b9b34e427ca
+// https://primereact.org/multiselect/
+// const userIds = selectedUsers.map(user => user.id);
