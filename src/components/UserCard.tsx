b2d20962a27effe87f569ca266f449cb85b885f8
Sacha Malaterre <203sach@gmail.com>
2025-06-13 11:40:21 +0200

    Update url avatar

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -8,7 +8,15 @@ interface UserCardProps{
 }
 
 export default function UserCard({firstname, lastname, avatar} : UserCardProps) : JSX.Element{
+
+    const backendBaseURL = "http://localhost:8080/uploads/avatars/";
+    const avatarUrl = avatar
+        ? `${backendBaseURL}${avatar}`
+        : `https://ui-avatars.com/api/?name=${encodeURIComponent(firstname + " " + lastname)}`;
+
+    console.log(avatarUrl)
     return(
+
             // j'ai fait gap:"10px" dans aside mais ca marche pas?
             <div style={{padding: "15px",
             marginBottom :"10px",
@@ -16,7 +24,7 @@ export default function UserCard({firstname, lastname, avatar} : UserCardProps) 
             }}>
                 <div>{firstname} {lastname}</div>
                 <img
-                        src={avatar}
+                    src={avatarUrl}
                     alt={`avatar...`}
                     style={{ width: "40px", height: "40px", borderRadius: "50%" }}
                 />
